859e846c0628b25f42775a688dd61a6bff1cad59
Shravan Jain <jainshravan123@gmail.com>
2016-09-25 03:23:26 +0530

    Quiz Maximum Part Completed except advance designing of Quiz.

--- app/src/main/java/heartbeat/social/tcs/socialhb/activity/modules/HeartBeatIndexModule.java
+++ app/src/main/java/heartbeat/social/tcs/socialhb/activity/modules/HeartBeatIndexModule.java
@@ -2,26 +2,64 @@ package heartbeat.social.tcs.socialhb.activity.modules;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 import heartbeat.social.tcs.socialhb.R;
+import heartbeat.social.tcs.socialhb.activity.Dashboard;
+import heartbeat.social.tcs.socialhb.activity.modules.quiz_activity.QuizQusAnsActivity;
 import heartbeat.social.tcs.socialhb.activity.modules.quiz_activity.QuizStartActivity;
+import heartbeat.social.tcs.socialhb.bean.CSRInit;
+import heartbeat.social.tcs.socialhb.bean.QuizScore;
+import heartbeat.social.tcs.socialhb.bean.Web_API_Config;
+import heartbeat.social.tcs.socialhb.sqliteDb.DBHelper;
+import heartbeat.social.tcs.socialhb.sqliteDb.QuizDBHelper;
 
 public class HeartBeatIndexModule extends AppCompatActivity {
 
-    Button btn_go_to_quiz;
+    AppCompatButton btn_start_quiz;
     Toolbar toolbar;
+    String TAG = "HeartBeatIndexModule";
+    SweetAlertDialog sAlertDialog;
+    QuizScore quizScore;
+    QuizScore prev_quizScore;
+    CSRInit prev_quiz_interestDetails;
+    ProgressBar progressBar;
+    LinearLayout linearLayout1;
+    LinearLayout linearLayout2;
+    TextView txt_quiz_highest_score, txt_quiz_interest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart_beat_index_module);
 
-        btn_go_to_quiz = (Button) findViewById(R.id.btn_go_to_quiz);
+        progressBar   = (ProgressBar) findViewById(R.id.prgBar);
+        linearLayout1 = (LinearLayout) findViewById(R.id.linearLayout1);
+        linearLayout2 = (LinearLayout) findViewById(R.id.linearLayout2);
+        txt_quiz_highest_score = (TextView) findViewById(R.id.txt_quiz_highest_score);
+        txt_quiz_interest = (TextView) findViewById(R.id.txt_quiz_interest);
+        btn_start_quiz = (AppCompatButton) findViewById(R.id.btn_start_quiz);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -31,18 +69,231 @@ public class HeartBeatIndexModule extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+
+                goBackActivity();
             }
         });
         getSupportActionBar().setTitle("Heartbeat Index");
 
 
-        btn_go_to_quiz.setOnClickListener(new View.OnClickListener() {
+        QuizDBHelper check_quizDBHelper = new QuizDBHelper(getApplicationContext());
+        if(check_quizDBHelper.checkQuizStartingDataExistence()){
+            check_quizDBHelper.deleteQuizTable();
+        }
+
+        btn_start_quiz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent1 = new Intent(getApplicationContext(), QuizStartActivity.class);
-                startActivity(intent1);
+
+                getQuiStartConfirmation();
+
+
             }
         });
+
+        try {
+            getScoreDetails();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getQuiStartConfirmation(){
+        SweetAlertDialog quiz_start_confirmation_dialog;
+        quiz_start_confirmation_dialog = new SweetAlertDialog(HeartBeatIndexModule.this, SweetAlertDialog.NORMAL_TYPE);
+        quiz_start_confirmation_dialog.setTitleText("Do you want to Start Quiz");
+        quiz_start_confirmation_dialog.setContentText("Are you Sure");
+        quiz_start_confirmation_dialog.show();
+
+        quiz_start_confirmation_dialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+            @Override
+            public void onClick(SweetAlertDialog sweetAlertDialog) {
+                startQuiz();
+            }
+        });
+    }
+
+    public void startQuiz()
+    {
+        sAlertDialog = new SweetAlertDialog(HeartBeatIndexModule.this, SweetAlertDialog.PROGRESS_TYPE);
+        sAlertDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        sAlertDialog.setTitleText("Authenticating...");
+        sAlertDialog.setCancelable(false);
+        sAlertDialog.show();
+
+        try {
+            getStartingQuizData();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getStartingQuizData() throws JSONException
+    {
+
+        String url = Web_API_Config.posting_starting_quiz_data;
+
+        DBHelper dbHelper =new DBHelper(getApplicationContext());
+        JSONObject user_json_obj = new JSONObject();
+        user_json_obj.put("id", String.valueOf(dbHelper.getUserID()));
+        user_json_obj.put("emp_id", String.valueOf(dbHelper.getEmpID()));
+
+        final JSONObject quiz_json_obj = new JSONObject();
+        quiz_json_obj.put("user", user_json_obj);
+
+
+        //Create JSONObjectRequest for Volley
+        JsonObjectRequest jsonRequest = new JsonObjectRequest
+                (Request.Method.POST, url, quiz_json_obj, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // the response is already constructed as a JSONObject!
+                        try {
+
+                            Log.e(TAG, response.toString());
+                            sAlertDialog.dismiss();
+
+                            quizScore = new QuizScore();
+                            quizScore.setArea_of_interest_cat_id(response.getInt("area_of_interest_cat_id"));
+                            quizScore.setId(response.getInt("id"));
+                            quizScore.setNo_of_qus(response.getInt("no_of_qus"));
+                            quizScore.setQuiz_id(response.getString("quiz_id"));
+                            quizScore.setScore(response.getInt("score"));
+                            quizScore.setStart_time(response.getString("start_time"));
+                            quizScore.setStatus(response.getInt("status"));
+
+                            QuizDBHelper quizDBHelper = new QuizDBHelper(getApplicationContext());
+                            quizDBHelper.addQuizStartingData(quizScore);
+
+                            Intent startQuizIntent = new Intent(getApplicationContext(), QuizQusAnsActivity.class);
+                            startActivity(startQuizIntent);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(jsonRequest);
+
+    }
+
+
+    public void getScoreDetails() throws JSONException
+    {
+
+
+        String url = Web_API_Config.get_quiz_prev_score_details;
+
+        DBHelper dbHelper =new DBHelper(getApplicationContext());
+        JSONObject user_json_obj = new JSONObject();
+        user_json_obj.put("id", String.valueOf(dbHelper.getUserID()));
+        user_json_obj.put("emp_id", String.valueOf(dbHelper.getEmpID()));
+
+        final JSONObject quiz_json_obj = new JSONObject();
+        quiz_json_obj.put("user", user_json_obj);
+
+
+        //Create JSONObjectRequest for Volley
+        JsonObjectRequest jsonRequest = new JsonObjectRequest
+                (Request.Method.POST, url, quiz_json_obj, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // the response is already constructed as a JSONObject!
+                        try {
+                            prev_quizScore = new QuizScore();
+                            prev_quizScore.setArea_of_interest_cat_id(response.getInt("area_of_interest_cat_id"));
+                            prev_quizScore.setCompletion_time(response.getString("completion_time"));
+                            prev_quizScore.setId(response.getInt("id"));
+                            prev_quizScore.setNo_of_qus(response.getInt("no_of_qus"));
+                            prev_quizScore.setQuiz_id(response.getString("quiz_id"));
+                            prev_quizScore.setScore(response.getInt("score"));
+                            prev_quizScore.setStart_time(response.getString("start_time"));
+                            prev_quizScore.setStatus(response.getInt("status"));
+
+
+                            getInterestCategoryName(prev_quizScore.getArea_of_interest_cat_id());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(jsonRequest);
+
+    }
+
+    public void getInterestCategoryName(int cat_id){
+
+        String url = Web_API_Config.csr_init_single_module + String.valueOf(cat_id);
+
+        //Create JSONObjectRequest for Volley
+        JsonObjectRequest jsonRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject csr_cat_response) {
+                        // the response is already constructed as a JSONObject!
+                        try {
+
+                            prev_quiz_interestDetails = new CSRInit();
+                            prev_quiz_interestDetails.setCsr_module_id(csr_cat_response.getInt("id"));
+                            prev_quiz_interestDetails.setCsr_module_name(csr_cat_response.getString("cat"));
+                            prev_quiz_interestDetails.setCsr_module_status(csr_cat_response.getInt("status"));
+                            prev_quiz_interestDetails.setCsr_module_icon(csr_cat_response.getString("cat_icon"));
+
+                            setData();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(jsonRequest);
+    }
+
+    public void setData()
+    {
+        progressBar.setVisibility(View.GONE);
+        linearLayout1.setVisibility(View.VISIBLE);
+        linearLayout2.setVisibility(View.VISIBLE);
+        btn_start_quiz.setVisibility(View.VISIBLE);
+
+        txt_quiz_highest_score.setText(String.valueOf(prev_quizScore.getScore()));
+        txt_quiz_interest.setText(String.valueOf(prev_quiz_interestDetails.getCsr_module_name()));
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        goBackActivity();
+    }
+
+    public void goBackActivity(){
+
+        Intent intent1 = new Intent(getApplicationContext(), Dashboard.class);
+        startActivity(intent1);
+
     }
 }
--- app/src/main/java/heartbeat/social/tcs/socialhb/activity/modules/quiz_activity/QuizQusAnsActivity.java
+++ app/src/main/java/heartbeat/social/tcs/socialhb/activity/modules/quiz_activity/QuizQusAnsActivity.java
@@ -1,5 +1,7 @@
 package heartbeat.social.tcs.socialhb.activity.modules.quiz_activity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
@@ -14,9 +16,12 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -26,18 +31,31 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
 import heartbeat.social.tcs.socialhb.R;
+import heartbeat.social.tcs.socialhb.activity.SignIn;
+import heartbeat.social.tcs.socialhb.activity.modules.HeartBeatIndexModule;
+import heartbeat.social.tcs.socialhb.bean.CSRInit;
+import heartbeat.social.tcs.socialhb.bean.Module;
 import heartbeat.social.tcs.socialhb.bean.QuizAnswer;
 import heartbeat.social.tcs.socialhb.bean.QuizQuestion;
 import heartbeat.social.tcs.socialhb.bean.QuizQuestionAnswer;
+import heartbeat.social.tcs.socialhb.bean.QuizScore;
+import heartbeat.social.tcs.socialhb.bean.QuizSelectedAnswer;
+import heartbeat.social.tcs.socialhb.bean.SignInUser;
+import heartbeat.social.tcs.socialhb.bean.User;
 import heartbeat.social.tcs.socialhb.bean.Web_API_Config;
+import heartbeat.social.tcs.socialhb.sqliteDb.DBHelper;
+import heartbeat.social.tcs.socialhb.sqliteDb.FirebaseTokenStorage;
 import heartbeat.social.tcs.socialhb.sqliteDb.QuizDBHelper;
+import heartbeat.social.tcs.socialhb.utility.ModuleSelector;
 
 public class QuizQusAnsActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     String TAG = "QuizQusAnsActivity";
     ArrayList<QuizQuestionAnswer> quiz_questions_list;
+    ArrayList<QuizSelectedAnswer> selected_question_answer_list;
     LinearLayout linearLayout1;
     ScrollView   scrollView1;
     ProgressBar  progressBar;
@@ -46,6 +64,8 @@ public class QuizQusAnsActivity extends AppCompatActivity {
     AppCompatButton btn_exit, btn_next;
     int question_number = 0;
     List<View> removeViews;
+    SweetAlertDialog submit_alert_dialog;
+    QuizScore quizScore_response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,14 +89,24 @@ public class QuizQusAnsActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                //finish();
+                quitFromQuiz();
             }
         });
         getSupportActionBar().setTitle("Quiz");
 
+        btn_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quitFromQuiz();
+            }
+        });
+
 
         QuizDBHelper quizDBHelper = new QuizDBHelper(getApplicationContext());
-        Toast.makeText(getApplicationContext(), "Quiz Records : " + quizDBHelper.getQuizStartDataCountTrial(), Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplicationContext(), "Quiz Records : " + quizDBHelper.getQuizStartDataCountTrial(), Toast.LENGTH_LONG).show();
+
+        selected_question_answer_list = new ArrayList<QuizSelectedAnswer>();
 
         try {
             getLimitedAmountQuizQus();
@@ -98,7 +128,7 @@ public class QuizQusAnsActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONArray response)
                     {
-                            Log.e(TAG, "Response : "+response.toString());
+                            Log.e(TAG, "Complete API Response : "+response.toString());
                             quiz_questions_list = new ArrayList<QuizQuestionAnswer>();
 
                             for(int i=0; i< response.length(); i++){
@@ -170,7 +200,7 @@ public class QuizQusAnsActivity extends AppCompatActivity {
 
     public void setFirstQuestionWithOptions(){
 
-        txt_question.setText(String.valueOf(question_number + 1) +". " + quiz_questions_list.get(question_number).getQuizQuestion().getQuestion().toString());
+        txt_question.setText(String.valueOf(question_number + 1) + ". " + quiz_questions_list.get(question_number).getQuizQuestion().getQuestion().toString());
         for(QuizAnswer quizAnswer : quiz_questions_list.get(question_number).getQuizAnswer_list()){
             AppCompatRadioButton appCompatRadioButton = new AppCompatRadioButton(this);
             appCompatRadioButton.setId(quizAnswer.getId());
@@ -186,18 +216,45 @@ public class QuizQusAnsActivity extends AppCompatActivity {
     public void setNextButtonEvent()
     {
 
-
             btn_next.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
 
-                    setQuestionWithOptions();
 
+                    int i = 0;
+                    for (QuizAnswer quizAnswer : quiz_questions_list.get(question_number - 1).getQuizAnswer_list()) {
+                        if (quizAnswer.getId() == radio_group_options.getCheckedRadioButtonId()) {
+                            i = 1;
+                        }
+                    }
+
+                    if (i == 1) {
+                        setQuestionWithOptions();
+                        Log.e(TAG, "Selected Answer ID   : " + radio_group_options.getCheckedRadioButtonId());
+                    } else {
+                        Log.e(TAG, "No Answer Selected");
+                        new SweetAlertDialog(QuizQusAnsActivity.this, SweetAlertDialog.ERROR_TYPE)
+                                .setTitleText("Please select one option")
+                                .setContentText("Try Again")
+                                .show();
+                    }
                 }
             });
     }
 
     public void setQuestionWithOptions(){
+
+
+        QuizQuestion selected_quizQuestion = new QuizQuestion();
+        QuizAnswer   selected_quizAnswer   = new QuizAnswer();
+        selected_quizQuestion.setId(quiz_questions_list.get(question_number-1).getQuizQuestion().getId());
+        selected_quizAnswer.setId(radio_group_options.getCheckedRadioButtonId());
+        QuizSelectedAnswer quizSelectedAnswer = new QuizSelectedAnswer();
+        quizSelectedAnswer.setQuizQuestion(selected_quizQuestion);
+        quizSelectedAnswer.setQuizAnswer(selected_quizAnswer);
+
+        selected_question_answer_list.add(quizSelectedAnswer);
+
 
         for(View radio_view : removeViews)
         {
@@ -207,12 +264,12 @@ public class QuizQusAnsActivity extends AppCompatActivity {
         removeViews.clear();
 
 
-        Log.e(TAG, "List Size "+quiz_questions_list.size());
+       // Log.e(TAG, "List Size "+quiz_questions_list.size());
         if(question_number < quiz_questions_list.size())
         {
             txt_question.setText(String.valueOf(question_number + 1) +". " + quiz_questions_list.get(question_number).getQuizQuestion().getQuestion().toString());
             for(QuizAnswer quizAnswer : quiz_questions_list.get(question_number).getQuizAnswer_list()){
-                Log.e(TAG, "Answer Before Print  : "+quizAnswer.getAnswer());
+               // Log.e(TAG, "Answer Before Print  : "+quizAnswer.getAnswer());
                 AppCompatRadioButton appCompatRadioButton = new AppCompatRadioButton(this);
                 appCompatRadioButton.setId(quizAnswer.getId());
                 appCompatRadioButton.setText(quizAnswer.getAnswer());
@@ -221,10 +278,195 @@ public class QuizQusAnsActivity extends AppCompatActivity {
                 removeViews.add(appCompatRadioButton);
             }
             question_number++;
+            if(question_number == quiz_questions_list.size()){
+                btn_next.setText("Submit");
+            }
         }else{
-            btn_next.setText("Submit");
+            /*try {
+                getQuizScore();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } */
+            txt_question.setText("");
+            submitQuiz();
+           // btn_next.setText("Submit");
         }
+
+
 
     }
 
+    public void submitQuiz()
+    {
+        submit_alert_dialog = new SweetAlertDialog(QuizQusAnsActivity.this, SweetAlertDialog.PROGRESS_TYPE);
+        submit_alert_dialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        submit_alert_dialog.setTitleText("Submitting Data...");
+        submit_alert_dialog.setCancelable(false);
+        submit_alert_dialog.show();
+
+        try {
+            getQuizScore();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public QuizScore getQuizScore() throws JSONException {
+
+        JSONArray jsonArray = new JSONArray();
+        for(QuizSelectedAnswer quizSelectedAnswer : selected_question_answer_list){
+
+            JSONObject jsonObject_qus_ans_obj = new JSONObject();
+
+            JSONObject jsonObject_qus_id = new JSONObject();
+            jsonObject_qus_id.put("id", quizSelectedAnswer.getQuizQuestion().getId());
+
+
+            JSONObject jsonObject_ans_id = new JSONObject();
+            jsonObject_ans_id.put("id", quizSelectedAnswer.getQuizAnswer().getId());
+
+            jsonObject_qus_ans_obj.put("quizQuestion", jsonObject_qus_id);
+            jsonObject_qus_ans_obj.put("quizAnswer", jsonObject_ans_id);
+
+            jsonArray.put(jsonObject_qus_ans_obj);
+        }
+
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("quizSelectedAnswers", jsonArray);
+
+       String url = Web_API_Config.calculating_quiz_score;
+
+        //Create JSONObjectRequest for Volley
+        JsonObjectRequest jsonRequest = new JsonObjectRequest
+                (Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject score_response) {
+                        // the response is already constructed as a JSONObject!
+                        try {
+                            Log.e(TAG, "Calculated Score Object : " + score_response);
+
+                            quizScore_response = new QuizScore();
+                            quizScore_response.setId(Integer.parseInt(String.valueOf(score_response.getInt("id"))));
+                            quizScore_response.setScore(Integer.parseInt(String.valueOf(score_response.getInt("score"))));
+                            quizScore_response.setArea_of_interest_cat_id(Integer.parseInt(String.valueOf(score_response.getInt("area_of_interest_cat_id"))));
+
+                            submitQuizScore();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(jsonRequest);
+        return quizScore_response;
+    }
+
+    public void submitQuizScore() throws JSONException {
+
+
+        DBHelper dbHelper = new DBHelper(getApplicationContext());
+        QuizDBHelper quizDBHelper = new QuizDBHelper(getApplicationContext());
+
+        JSONObject user_jsonObject = new JSONObject();
+        user_jsonObject.put("id", dbHelper.getUserID());
+        user_jsonObject.put("emp_id", dbHelper.getEmpID());
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("quiz_id", quizDBHelper.getQuizStartingData().getQuiz_id());
+        jsonObject.put("user", user_jsonObject);
+        jsonObject.put("score", quizScore_response.getScore());
+        jsonObject.put("start_time", "06/09/16 01:06:11");
+        jsonObject.put("status", 1);
+        jsonObject.put("no_of_qus", quizDBHelper.getQuizStartingData().getNo_of_qus());
+        jsonObject.put("area_of_interest_cat_id", quizScore_response.getArea_of_interest_cat_id());
+
+        String url = Web_API_Config.posting_final_quiz_score;
+
+        //Create JSONObjectRequest for Volley
+        JsonObjectRequest jsonRequest = new JsonObjectRequest
+                (Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // the response is already constructed as a JSONObject!
+                        try {
+
+                            if(Integer.parseInt(String.valueOf(response.getInt("status"))) == 1)
+                            {
+                                showSuccessMessage();
+                            }else{
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(jsonRequest);
+
+
+
+
+    }
+
+    public void showSuccessMessage()
+    {
+        submit_alert_dialog.cancel();
+        SweetAlertDialog quiz_submit_successfull_dialog;
+        quiz_submit_successfull_dialog = new SweetAlertDialog(QuizQusAnsActivity.this, SweetAlertDialog.SUCCESS_TYPE);
+        quiz_submit_successfull_dialog.setTitleText("Successfully Completed Quiz");
+        quiz_submit_successfull_dialog.setContentText("Score : " + quizScore_response.getScore());
+        quiz_submit_successfull_dialog.show();
+
+        quiz_submit_successfull_dialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+            @Override
+            public void onClick(SweetAlertDialog sweetAlertDialog) {
+                Intent quizStartIntent = new Intent(getApplicationContext(), HeartBeatIndexModule.class);
+                startActivity(quizStartIntent);
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        quitFromQuiz();
+
+    }
+
+    public void quitFromQuiz()
+    {
+        SweetAlertDialog quiz_quit_dialog;
+        quiz_quit_dialog = new SweetAlertDialog(QuizQusAnsActivity.this, SweetAlertDialog.WARNING_TYPE);
+        quiz_quit_dialog.setTitleText("Do you want to Quit");
+        quiz_quit_dialog.setContentText("Are you Sure");
+        quiz_quit_dialog.show();
+
+        quiz_quit_dialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+            @Override
+            public void onClick(SweetAlertDialog sweetAlertDialog) {
+
+                QuizDBHelper quizDBHelper = new QuizDBHelper(getApplicationContext());
+                quizDBHelper.deleteQuizTable();
+
+                Intent quizStartIntent = new Intent(getApplicationContext(), HeartBeatIndexModule.class);
+                startActivity(quizStartIntent);
+            }
+        });
+    }
 }
